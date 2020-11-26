94646f53c3f48f4f3179e2e14745b2bf982e2a69
gr33man <frman744@gmail.com>
2020-11-26 19:18:03 +0400

    add dist

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
         form: './form/form.js'
     },
     output: {
-        filename: '[name].js',
+        filename: './[name]/[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
 
